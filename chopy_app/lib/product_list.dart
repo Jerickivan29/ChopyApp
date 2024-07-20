3f42ddfa08d7bdd76aac89c7e72249b29d990590
russel-122 <tulipat9@gmail.com>
2024-07-20 14:35:00 +0800

    Add product list

---
+++ chopy_app/lib/product_list.dart
@@ -0,0 +1,17 @@
+class ProductList {
+
+  const ProductList({required this.name, required this.category, required this.description, required this.price});
+
+  final String name;
+  final String category;
+  final String description;
+  final int price;
+
+}
+
+List<ProductList> products = [
+  const ProductList(name: "Samsung 10", category: "Gadgets", description: "dhfksdjh ", price: 2000),
+  const ProductList(name: "Laptop", category: "Gadgets", description: "dhfksdjh ", price: 2000),
+  const ProductList(name: "Pencil", category: "school", description: "dhfksdjh ", price: 2000),
+  const ProductList(name: "Paper", category: "school", description: "dhfksdjh ", price: 2000),
+];
